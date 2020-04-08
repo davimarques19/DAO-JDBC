a5e48900e9ab07f84ce0214aff22e5a03f937a2d
Davi Marques <daviestudos19@gmail.com>
2020-04-08 10:21:27 -0300

    findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Book");
-		Seller seller = new Seller(1, "Davis", "davi@email.com", new Date(), 5000.0, obj);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
+		Seller seller = sellerDao.findById(1);
+
 		System.out.println(seller);
 
 	}
